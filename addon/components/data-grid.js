c65724e17ac9e1f37464a3aad81ec4316d71f93e
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-09-29 15:20:17 +0200

    add easy pagination

--- addon/components/data-grid.js
+++ addon/components/data-grid.js
@@ -29,15 +29,57 @@ function getPropertiesList(properties) {
 
 export default Ember.Component.extend({
   layout: layout,
-  tagName: 'table',
   classNames: ['data-grid'],
-  attributeBindings: ['width'],
+  attributeBindings: ['widthString:style'],
 
+  currentPage: 1,
   showHeader: true,
+
+  widthString: computed('width', function() {
+    const width = this.get('width') + '';
+    if (width.indexOf('%') > 0) { return width; }
+
+    return  'width: ' + width + 'px';
+  }),
 
   propertiesList: computed('properties', function() {
     const properties = this.get('properties');
 
     return getPropertiesList(properties);
-  })
+  }),
+
+  pageAmount: computed('pageSize', 'content.length', function() {
+    return Math.ceil(this.get('content.length') / this.get('pageSize'));
+  }),
+
+  pageNumbers: computed('pageAmount', function() {
+    const pages = [];
+
+    for (let i = 1; i <= this.get('pageAmount'); i++) {
+      pages.push(i);
+    }
+
+    return pages;
+  }),
+
+  pageContent: computed('pageSize', 'currentPage', 'content.@each', function() {
+    const pageSize    = this.get('pageSize');
+    const currentPage = this.get('currentPage');
+    const content     = this.get('content');
+
+    if (pageSize && pageSize < content.length) {
+      const start = 0 + ((currentPage - 1) * pageSize);
+      const end = start + pageSize;
+
+      return content.slice(start, end);
+    }
+
+    return content;
+  }),
+
+  actions: {
+    goToPage(pageNr) {
+      this.set('currentPage', pageNr);
+    }
+  }
 });
--- tests/integration/components/data-grid-test.js
+++ tests/integration/components/data-grid-test.js
@@ -2,35 +2,40 @@ import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 import { moduleForComponent, test } from 'ember-qunit';
 
-moduleForComponent('data-grid', 'Integration | Component | {{data-grid}}', {
-  integration: true,
-  beforeEach: function() {
-    const list = [
-      Ember.Object.create({
-        id: 1,
-        name: 'Ben',
-        type: 'dog'
-      }),
-      Ember.Object.create({
-        id: 2,
-        name: 'Alex',
-        type: 'dog'
-      }),
-      Ember.Object.create({
-        id: 3,
-        name: 'Chris',
-        type: 'cat'
-      })
-    ];
+function buildSmallList() {
+  return [
+    Ember.Object.create({ id: 1, name: 'Ben', type: 'dog' }),
+    Ember.Object.create({ id: 2, name: 'Alex', type: 'dog' }),
+    Ember.Object.create({ id: 3, name: 'Chris', type: 'cat' })
+  ];
+}
 
-    this.set('list', list);
+function buildLargeList(length) {
+  const list = [];
+
+  for (let i = 1; i <= length;i++) {
+    const object = Ember.Object.create({
+      id:   i,
+      name: 'Name' + i,
+      type: 'type' + i
+    });
+
+    list.push(object);
   }
+
+  return list;
+}
+
+moduleForComponent('data-grid', 'Integration | Component | {{data-grid}}', {
+  integration: true
 });
 
 test('renders the table with given content', function(assert) {
+  this.set('list', buildSmallList());
+
   this.render(hbs`{{data-grid content=list}}`);
 
-  const $table = this.$('table.data-grid');
+  const $table = this.$('.data-grid table');
   const $rows  = $table.find('tbody tr');
 
   assert.equal($table.length, 1, 'renders table');
@@ -38,13 +43,14 @@ test('renders the table with given content', function(assert) {
 });
 
 test('renders with display options', function(assert) {
+  this.set('list', buildSmallList());
   this.set('showHeader', true);
 
   this.render(hbs`{{data-grid content=list
                               showHeader=showHeader
                               width=800}}`);
 
-  const $table = this.$('table.data-grid');
+  const $table = this.$('.data-grid table');
   let $header  = $table.find('thead');
 
   assert.equal($header.length, 1, 'shows header by default');
@@ -57,10 +63,12 @@ test('renders with display options', function(assert) {
 });
 
 test('shows label and value for properties string', function(assert) {
+  this.set('list', buildSmallList());
+
   this.render(hbs`{{data-grid content=list
                               properties="id name type"}}`);
 
-  const $table        = this.$('table.data-grid');
+  const $table        = this.$('.data-grid table');
   const $headerColls  = $table.find('thead tr').eq(0).find('th');
   const $contentColls = $table.find('tbody tr').eq(0).find('td');
 
@@ -68,9 +76,9 @@ test('shows label and value for properties string', function(assert) {
   assert.equal($headerColls[1].innerText.toUpperCase(), 'NAME');
   assert.equal($headerColls[2].innerText.toUpperCase(), 'TYPE');
 
-  assert.equal($contentColls[0].innerText.toUpperCase(), '1');
-  assert.equal($contentColls[1].innerText.toUpperCase(), 'BEN');
-  assert.equal($contentColls[2].innerText.toUpperCase(), 'DOG');
+  assert.equal($contentColls[0].innerText, '1');
+  assert.equal($contentColls[1].innerText, 'Ben');
+  assert.equal($contentColls[2].innerText, 'dog');
 });
 
 test('shows label and value for properties array with objects', function(assert) {
@@ -80,11 +88,12 @@ test('shows label and value for properties array with objects', function(assert)
     { key: 'type', label: 'species' }
   ];
 
+  this.set('list', buildSmallList());
   this.set('properties', properties);
   this.render(hbs`{{data-grid content=list
                               properties=properties}}`);
 
-  const $table        = this.$('table.data-grid');
+  const $table        = this.$('.data-grid table');
   const $headerColls  = $table.find('thead tr').eq(0).find('th');
   const $contentColls = $table.find('tbody tr').eq(0).find('td');
 
@@ -92,7 +101,40 @@ test('shows label and value for properties array with objects', function(assert)
   assert.equal($headerColls[1].innerText.toUpperCase(), 'NAME');
   assert.equal($headerColls[2].innerText.toUpperCase(), 'SPECIES');
 
-  assert.equal($contentColls[0].innerText.toUpperCase(), '1');
-  assert.equal($contentColls[1].innerText.toUpperCase(), 'BEN');
-  assert.equal($contentColls[2].innerText.toUpperCase(), 'DOG');
+  assert.equal($contentColls[0].innerText, '1');
+  assert.equal($contentColls[1].innerText, 'Ben');
+  assert.equal($contentColls[2].innerText, 'dog');
+});
+
+test('pagination', function(assert) {
+  this.set('list', buildLargeList(12));
+  this.set('pageSize', 5);
+
+  this.render(hbs`{{data-grid content=list
+                              properties="id name type"
+                              pageSize=pageSize}}`);
+
+  const $table  = this.$('.data-grid table');
+  let $rows     = $table.find('tbody tr');
+  let $firstRow = $rows.eq(0).find('td');
+
+  assert.equal($rows.length, 5, 'shows pageSize limited content');
+  assert.equal($firstRow[0].innerText, '1',
+              'first page starts with first element');
+
+  const $pages        = this.$('.data-grid__paging .data-grid__paging__nr');
+  const $selectedPage = this.$('.data-grid__paging__nr--selected');
+
+  assert.equal($pages.length, 3, 'shows paging buttons');
+  assert.equal($selectedPage.length, 1, 'just one page selected');
+  assert.equal($selectedPage[0].innerText, '1', 'first page selected by default');
+
+  $pages.eq(2).trigger('click');
+
+  $rows     = $table.find('tbody tr');
+  $firstRow = $rows.eq(0).find('td');
+
+  assert.equal($rows.length, 2, 'shows end content');
+  assert.equal($firstRow[0].innerText, '11',
+              'first page starts with next element');
 });
