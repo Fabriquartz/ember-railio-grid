e9223634ffa4c943f2c3a4ee89e453a9287c34ef
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-10-15 18:16:12 +0200

    modify data-manager to just have the handlers

--- addon/utils/data-manager.js
+++ addon/utils/data-manager.js
@@ -1,70 +1,39 @@
 import Ember from 'ember';
 
-import Filterer  from 'ember-railio-grid/utils/filterer';
-import Sorter    from 'ember-railio-grid/utils/sorter';
-import Paginator from 'ember-railio-grid/utils/paginator';
+import PaginatingHandler from 'ember-railio-grid/utils/paginating-handler';
+import SortingHandler    from 'ember-railio-grid/utils/sorting-handler';
+import FilteringHandler  from 'ember-railio-grid/utils/filtering-handler';
 
 const { computed } = Ember;
-const { alias } = computed;
 
 export default Ember.Object.extend({
-  paginator: computed({
+  filteringHandler: computed({
     set(key, value) {
-      this.set('_paginator', value);
+      this.set('_filteringHandler', value);
       return value;
     },
     get() {
-      return this.get('_paginator') || Paginator.create();
+      return this.get('_filteringHandler') || FilteringHandler.create();
     }
   }),
 
-  sorter: computed({
+  sortingHandler: computed({
     set(key, value) {
-      this.set('_sorter', value);
+      this.set('_sortingHandler', value);
       return value;
     },
     get() {
-      return this.get('_sorter') || Sorter.create();
+      return this.get('_sortingHandler') || SortingHandler.create();
     }
   }),
 
-  filterer: computed({
+  paginatingHandler: computed({
     set(key, value) {
-      this.set('_filterer', value);
+      this.set('_paginatingHandler', value);
       return value;
     },
     get() {
-      return this.get('_filterer') || Filterer.create();
+      return this.get('_paginatingHandler') || PaginatingHandler.create();
     }
-  }),
-
-  init: function() {
-    this._bindContentToFilterer();
-    this._bindFilteredToSorter();
-    this._bindSortedToPaginator();
-    this._super(...arguments);
-  },
-
-  _bindContentToFilterer() {
-    let binding = this.get('contentFiltererBinding');
-    if (binding  != null) { binding.disconnect(this); }
-    binding = Ember.bind(this, 'filterer.content', 'content');
-    this.set('contentFiltererBinding', binding);
-  },
-
-  _bindFilteredToSorter() {
-    let binding = this.get('filtererSorterBinding');
-    if (binding  != null) { binding.disconnect(this); }
-    binding = Ember.bind(this, 'sorter.content', 'filterer.filteredContent');
-    this.set('filtererSorterBinding', binding);
-  },
-
-  _bindSortedToPaginator() {
-    let binding = this.get('sorterPaginatorBinding');
-    if (binding  != null) { binding.disconnect(this); }
-    binding = Ember.bind(this, 'paginator.content', 'sorter.sortedContent');
-    this.set('sorterPaginatorBinding', binding);
-  },
-
-  managedContent: alias('paginator.currentPage')
+  })
 });
--- tests/unit/utils/data-manager-test.js
+++ tests/unit/utils/data-manager-test.js
@@ -1,82 +1,98 @@
-import Ember       from 'ember';
-import DataManager from 'ember-railio-grid/utils/data-manager';
-import Paginator   from 'ember-railio-grid/utils/paginator';
-import Sorter      from 'ember-railio-grid/utils/sorter';
-import Filterer    from 'ember-railio-grid/utils/filterer';
+import DataManager       from 'ember-railio-grid/utils/data-manager';
+import PaginatingHandler from 'ember-railio-grid/utils/paginating-handler';
+import SortingHandler    from 'ember-railio-grid/utils/sorting-handler';
+import FilteringHandler  from 'ember-railio-grid/utils/filtering-handler';
 
 import { module, test } from 'qunit';
 
-const { run } = Ember;
+let dataManager;
 
-module('Unit | Utility | data-manager');
-
-test('create a data-manager with content array', function(assert) {
-  const dataManager = DataManager.create({
-    content: [1, 2, 3, 4]
-  });
-
-  assert.deepEqual(dataManager.get('managedContent'), [1, 2, 3, 4]);
+module('Unit | Utility | data-manager', {
+  beforeEach: function() {
+    dataManager = DataManager.create({
+      content: [1, 2, 3, 4]
+    });
+  }
 });
 
-test('creates a default paginator with content', function(assert) {
-  const dataManager = DataManager.create({
-    content: [1, 2, 3, 4]
-  });
-
-  const paginator = dataManager.get('paginator');
-
-  assert.notEqual(paginator, null);
-  assert.ok(paginator instanceof Paginator);
-
-  assert.deepEqual(paginator.get('content'), [1, 2, 3, 4]);
+test('creates a filtering-handler', function(assert) {
+  const handler = dataManager.get('filteringHandler');
+  assert.notEqual(handler, null);
+  assert.ok(handler instanceof FilteringHandler);
 });
 
-test('creates a default sorter with content', function(assert) {
-  const dataManager = DataManager.create({
-    content: [1, 2, 3, 4]
-  });
-
-  const sorter = dataManager.get('sorter');
-
-  assert.notEqual(sorter, null);
-  assert.ok(sorter instanceof Sorter);
-  assert.deepEqual(sorter.get('content'), [1, 2, 3, 4]);
+test('creates a sorting-handler', function(assert) {
+  const handler = dataManager.get('sortingHandler');
+  assert.notEqual(handler, null);
+  assert.ok(handler instanceof SortingHandler);
 });
 
-test('creates a default filter with content', function(assert) {
-  const dataManager = DataManager.create({
-    content: [1, 2, 3, 4]
-  });
-
-  const filterer = dataManager.get('filterer');
-
-  assert.notEqual(filterer, null);
-  assert.ok(filterer instanceof Filterer);
-  assert.deepEqual(filterer.get('content'), [1, 2, 3, 4]);
+test('creates a paginating-handler', function(assert) {
+  const handler = dataManager.get('paginatingHandler');
+  assert.notEqual(handler, null);
+  assert.ok(handler instanceof PaginatingHandler);
 });
 
-test('managedContent modified by filter, sorter and paginator', function(assert) {
-  const item1 = { id: 1, name: 'Frank' };
-  const item2 = { id: 2, name: 'Chris' };
-  const item3 = { id: 3, name: 'George' };
-  const item4 = { id: 4, name: 'Alex' };
-  const item5 = { id: 5, name: 'Dilan' };
-
-  const dataManager = DataManager.create({
-    content: [ item1, item2, item3, item4, item5]
-  });
-
-  run(() => {
-    dataManager.get('filterer').addFilter('name', 'contains', 'a');
-    dataManager.get('sorter').addSortKey('name');
-    dataManager.set('paginator.pageSize', 2);
-  });
-
-  assert.deepEqual(dataManager.get('managedContent'), [item4, item5]);
-
-  run(() => {
-    dataManager.set('paginator.page', 2);
-  });
-
-  assert.deepEqual(dataManager.get('managedContent'), [item1]);
-});
+//
+// test('creates a default paginator with content', function(assert) {
+//   const dataManager = DataManager.create({
+//     content: [1, 2, 3, 4]
+//   });
+//
+//   const paginator = dataManager.get('paginator');
+//
+//   assert.notEqual(paginator, null);
+//   assert.ok(paginator instanceof Paginator);
+//
+//   assert.deepEqual(paginator.get('content'), [1, 2, 3, 4]);
+// });
+//
+// test('creates a default sorter with content', function(assert) {
+//   const dataManager = DataManager.create({
+//     content: [1, 2, 3, 4]
+//   });
+//
+//   const sorter = dataManager.get('sorter');
+//
+//   assert.notEqual(sorter, null);
+//   assert.ok(sorter instanceof Sorter);
+//   assert.deepEqual(sorter.get('content'), [1, 2, 3, 4]);
+// });
+//
+// test('creates a default filter with content', function(assert) {
+//   const dataManager = DataManager.create({
+//     content: [1, 2, 3, 4]
+//   });
+//
+//   const filterer = dataManager.get('filterer');
+//
+//   assert.notEqual(filterer, null);
+//   assert.ok(filterer instanceof Filterer);
+//   assert.deepEqual(filterer.get('content'), [1, 2, 3, 4]);
+// });
+//
+// test('managedContent modified by filter, sorter and paginator', function(assert) {
+//   const item1 = { id: 1, name: 'Frank' };
+//   const item2 = { id: 2, name: 'Chris' };
+//   const item3 = { id: 3, name: 'George' };
+//   const item4 = { id: 4, name: 'Alex' };
+//   const item5 = { id: 5, name: 'Dilan' };
+//
+//   const dataManager = DataManager.create({
+//     content: [ item1, item2, item3, item4, item5]
+//   });
+//
+//   run(() => {
+//     dataManager.get('filterer').addFilter('name', 'contains', 'a');
+//     dataManager.get('sorter').addSortKey('name');
+//     dataManager.set('paginator.pageSize', 2);
+//   });
+//
+//   assert.deepEqual(dataManager.get('managedContent'), [item4, item5]);
+//
+//   run(() => {
+//     dataManager.set('paginator.page', 2);
+//   });
+//
+//   assert.deepEqual(dataManager.get('managedContent'), [item1]);
+// });
