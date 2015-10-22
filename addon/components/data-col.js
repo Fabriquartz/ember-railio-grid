ad45a9111f8bbe8658d816dbff329864ae5fbb73
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-10-22 11:11:42 +0200

    add optional styling for cell

--- addon/components/data-col.js
+++ addon/components/data-col.js
@@ -5,14 +5,67 @@ const { computed, defineProperty } = Ember;
 const { reads } = computed;
 
 const defaultPropertyObject = {
-  format: function(value) { return value; }
+  format: function(value) { return value; },
+  style: {
+    width:           'auto',
+
+    horizontalAlign: 'inherit',
+    verticalAlign:   'inherit',
+
+    backgroundColor: 'inherit',
+
+    fontFamily:      'inherit',
+    fontWeight:      'inherit',
+    italic:          false,
+    fontColor:       'inherit',
+
+    borderWidth:     3,
+    borderColor:     'inherit',
+    borderStyle:     'inherit'
+  }
 };
+
+function copy(object) {
+  return  $.extend({}, object);
+}
 
 export default Ember.Component.extend({
   tagName: 'td',
+  attributeBindings: ['style'],
 
   _property: computed('property', function() {
     return $.extend({}, defaultPropertyObject, this.get('property'));
+  }),
+
+  style: computed('_value', '_property.style',
+  function() {
+    const value = this.get('_value');
+    const style = copy(this.get('_property.style'));
+
+    // if style property is a function, return style depending on value
+    for (const property in style) {
+      if (style.hasOwnProperty(property) &&
+          typeof style[property] === 'function') {
+        style[property] = style[property](value);
+      }
+      if (typeof style[property] === 'boolean') {
+        style[property] = style[property] ? property : 'inherit';
+      }
+    }
+
+    return `
+      width:            ${style.width}em;
+      text-align:       ${style.horizontalAlign};
+      vertical-align:   ${style.verticalAlign};
+      background-color: ${style.backgroundColor};
+      font-family:      ${style.fontFamily};
+      font-weight:      ${style.fontWeight};
+      font-style:       ${style.italic};
+      color:            ${style.fontColor};
+      border-width:     ${style.borderWidth}px;
+      border-color:     ${style.borderColor};
+      border-style:     ${style.borderStyle};
+    `;
   }),
 
   value: computed('_value', '_property.{format}', function() {
--- tests/integration/components/data-col-test.js
+++ tests/integration/components/data-col-test.js
@@ -28,3 +28,71 @@ test('formats the value by the format function', function(assert) {
 
   assert.equal(this.$('td')[0].innerText, 'name: Foo Bar');
 });
+
+test('styles the cell with given styling', function(assert) {
+  this.set('item', { id: 1, name: 'Foo' });
+  this.set('property', {
+    key:   'name',
+    label: 'animal name',
+    style:  {
+      width:           20,
+
+      horizontalAlign: 'center',
+      verticalAlign:   'bottom',
+
+      backgroundColor: 'red',
+
+      fontFamily:      'Sans',
+      fontWeight:      300,
+      italic:          true,
+      fontColor:       'green',
+
+      borderWidth:     4,
+      borderColor:     'blue',
+      borderStyle:     'dotted'
+    }
+  });
+
+  this.render(hbs`{{data-col item=item property=property}}`);
+
+  const $cell = this.$('td').eq(0);
+
+  const em    = parseInt($cell.css('font-size').replace('px', ''));
+  const width = 20 * em;
+
+  assert.equal($cell.css('width'),            `${width}px`,     'width');
+  assert.equal($cell.css('text-align'),       'center',         'horizontalAlign');
+  assert.equal($cell.css('vertical-align'),   'bottom',         'verticalAlign');
+  assert.equal($cell.css('background-color'), 'rgb(255, 0, 0)', 'backgroundColor');
+  assert.equal($cell.css('font-family'),      'Sans',           'fontFamily');
+  assert.equal($cell.css('font-weight'),      '300',            'fontWeight');
+  assert.equal($cell.css('font-style'),       'italic',         'italic');
+  assert.equal($cell.css('color'),            'rgb(0, 128, 0)', 'fontColor');
+  assert.equal($cell.css('border-width'),     '4px',            'borderWidth');
+  assert.equal($cell.css('border-color'),     'rgb(0, 0, 255)', 'borderColor');
+  assert.equal($cell.css('border-style'),     'dotted',         'borderStyle');
+});
+
+test('styles the cell depending on value', function(assert) {
+  this.set('item', { name: 'Foo', age: 5 });
+  this.set('property', {
+    key:   'age',
+    label: 'Age',
+    style:  {
+      backgroundColor: function(value) {
+        if (value > 0) {
+          return 'green';
+        }
+        return 'red';
+      }
+    }
+  });
+
+  this.render(hbs`{{data-col item=item property=property}}`);
+
+  const $cell = this.$('td').eq(0);
+  assert.equal($cell.css('background-color'), 'rgb(0, 128, 0)');
+
+  this.set('item.age', -2);
+  assert.equal($cell.css('background-color'), 'rgb(255, 0, 0)');
+});
