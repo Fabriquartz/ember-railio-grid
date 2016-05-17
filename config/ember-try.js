71aa94d8bd4a3e13e4ecee5524c592415095306a
Chantal Broeren <chantal.broeren@fabriquartz.com>
2016-05-17 13:46:15 +0200

    add ember-try scenario's

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,45 +1,76 @@
 module.exports = {
   scenarios: [
     {
+      name: 'default',
+      dependencies: { }
+    },
+    {
       name: 'ember-1.13.X',
       dependencies: {
-        'ember': '1.13.13'
+        'ember': '~1.13.0'
       }
     },
     {
       name: 'ember-2.0.X',
       dependencies: {
-        'ember': '2.0.3'
+        'ember': '~2.0.0'
       }
     },
     {
       name: 'ember-2.1.X',
       dependencies: {
-        'ember': '2.1.2'
+        'ember': '~2.1.0'
       }
     },
     {
       name: 'ember-2.2.X',
       dependencies: {
-        'ember': '2.2.2'
+        'ember': '~2.2.0'
       }
     },
     {
       name: 'ember-2.3.X',
       dependencies: {
-        'ember': '2.3.1'
+        'ember': '~2.3.0'
       }
     },
     {
       name: 'ember-2.4.X',
       dependencies: {
-        'ember': '2.4.1'
+        'ember': '~2.4.0'
       }
     },
     {
       name: 'ember-2.5.X',
       dependencies: {
-        'ember': '2.5.1'
+        'ember': '~2.5.0'
+      }
+    },
+    {
+      name: 'ember-release',
+      dependencies: {
+        'ember': 'components/ember#release'
+      },
+      resolutions: {
+        'ember': 'release'
+      }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    },
+    {
+      name: 'ember-canary',
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
       }
     }
   ]
