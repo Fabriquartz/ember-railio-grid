b17e62aa4a4b42b748e22cbb857d0acd8a826616
Chantal Broeren <chantal.broeren@fabriquartz.com>
2017-09-26 16:56:06 +0200

    refactor and improve sorter

--- addon/utils/sorter.js
+++ addon/utils/sorter.js
@@ -1,42 +1,18 @@
-import Ember from 'ember';
-import EmberObject from 'ember-object';
-
-import computed from 'ember-computed';
-import get from 'ember-metal/get';
-
-const { compare } = Ember;
-
-function orderBy(list, sortProperties) {
-  if (!sortProperties || sortProperties.length === 0) {
-    return list;
-  }
-
-  // Bug/workaround: Ember.ArrayProxy has no method sort.
-  // see: https://github.com/emberjs/ember.js/issues/11936
-  if (typeof list.sort !== 'function' && typeof list.toArray === 'function') {
-    list = list.toArray();
-  }
-
-  // [].concat(list) = clone array
-  return [].concat(list).sort((item1, item2) => {
-    let result = 0;
-
-    sortProperties.forEach((property) => {
-      if (result === 0) {
-        result = compare(get(item1, property.key), get(item2, property.key));
-        if (result !== 0 && property.descending) {
-          result = result * -1;
-        }
-      }
-    });
-
-    return result;
-  });
-}
+import EmberObject        from 'ember-object';
+import computed, { sort } from 'ember-computed';
+import get                from 'ember-metal/get';
 
 export default EmberObject.extend({
-  sortedContent: computed('content.[]', 'handler.sortKeys.@each.{key,descending}',
+  sortKeys: computed('content.[]', 'handler.sortKeys.@each.{key,descending}',
   function() {
-    return orderBy(get(this, 'content'), get(this, 'handler.sortKeys'));
-  })
+    let list = get(this, 'handler.sortKeys').map((sortKey) => {
+      let key        = get(sortKey, 'key');
+      let descending = get(sortKey, 'descending') ? ':desc' : '';
+      return `${key}${descending}`;
+    });
+
+    return list;
+  }),
+
+  sortedContent: sort('content', 'sortKeys')
 });
