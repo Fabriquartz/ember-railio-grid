d1bed711594a91426cac1d0df44e290f4fb84249
Arne Diehl <arne.diehl@fabriquartz.com>
2017-12-12 12:11:05 +0100

    fix ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,19 +3,13 @@ module.exports = {
   scenarios: [
     {
       name: 'ember-lts-2.4',
-      bower: {
-        dependencies: { 'ember': 'components/ember#lts-2-4' },
-        resolutions: { 'ember': 'lts-2-4' }
-      },
-      npm: { devDependencies: { 'ember-source': null } }
+      bower: {},
+      npm: { devDependencies: { 'ember-source': '~2.4.0' } }
     },
     {
       name: 'ember-lts-2.8',
-      bower: {
-        dependencies: { 'ember': 'components/ember#lts-2-8' },
-        resolutions: { 'ember': 'lts-2-8' }
-      },
-      npm: { devDependencies: { 'ember-source': null } }
+      bower: {},
+      npm: { devDependencies: { 'ember-source': '~2.8.0' } }
     },
     {
       name: 'ember-lts-2.12',
