77d2ce6e6ea63e82ff109fe8a1328be1dabc4b0e
Heng <tenn_lh@hotmail.com>
2018-01-31 11:24:59 +0100

    upgrade ember to v2.18 (#33)

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,19 +1,17 @@
 import Application from '../../app';
-import config      from '../../config/environment';
-import { assign }  from 'ember-platform';
-import { run }     from '@ember/runloop';
+import config from '../../config/environment';
+import { merge } from '@ember/polyfills';
+import { run } from '@ember/runloop';
 
 export default function startApp(attrs) {
-  let application;
+  let attributes = merge({}, config.APP);
+  attributes.autoboot = true;
+  attributes = merge(attributes, attrs); // use defaults, but you can override;
 
-  let attributes = assign({}, config.APP);
-  attributes = assign(attributes, attrs); // use defaults, but you can override;
-
-  run(function() {
-    application = Application.create(attributes);
+  return run(() => {
+    let application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
+    return application;
   });
-
-  return application;
 }
