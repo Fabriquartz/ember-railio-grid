b4c2adeac23f72414951c27d16f7ad94c56c266c
Chantal Broeren <chantal.broeren@fabriquartz.com>
2018-05-01 16:46:22 +0200

    refactor bindings with Ember.bind to computed solutions

--- addon/utils/paginator.js
+++ addon/utils/paginator.js
@@ -3,14 +3,16 @@ import EmberObject from 'ember-object';
 import computed from 'ember-computed';
 import get from 'ember-metal/get';
 
-export default EmberObject.extend({
+import ContentContextMixin from '../mixins/content-context';
+
+export default EmberObject.extend(ContentContextMixin, {
   currentPage: computed('handler.page', 'handler.pageSize', 'content.[]',
   function() {
     let page = get(this, 'handler.page');
     let pageSize = get(this, 'handler.pageSize');
     let content = get(this, 'content');
 
-    if (pageSize && pageSize < content.length) {
+    if (pageSize && content && pageSize < get(content, 'length')) {
       let start = 0 + ((page - 1) * pageSize);
       let end = start + pageSize;
 
